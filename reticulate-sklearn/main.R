5f4433ddb35a177f164f4b0decc26307525419b6
Jake Mason <jake-mason@users.noreply.github.com>
2020-05-19 14:16:25 -0500

    Add create_column_transformer

--- reticulate-sklearn/main.R
+++ reticulate-sklearn/main.R
@@ -5,7 +5,7 @@ library(dplyr)
 
 # Create virtualenv for R to use. Note: I suppose you could also use this https://rstudio.github.io/reticulate/articles/python_packages.html
 system('mkdir ~/.virtualenvs')
-system('virtualenv ~/.virtualenvs/venv')
+system('python3 -m venv ~/.virtualenvs/venv')
 system('source ~/.virtualenvs/venv/bin/activate && pip3 install pandas numpy scikit-learn xgboost && deactivate')
 
 use_virtualenv('venv')
@@ -20,16 +20,32 @@ data(mtcars)
 
 df <- mtcars
 
-# Attempt #1
-transformers <- list(
-	c('mpg', sk_preprocessing$StandardScaler(), tuple('mpg')),
-	c('wt', 'passthrough', tuple('wt')),
-	c('gear', sk_preprocessing$OneHotEncoder(), tuple('gear'))
-	# TODO: handle SimpleImputer, too?
+feature_mapping <- data.frame(
+	parent_feature = c('vs', 'mpg', 'gear'),
+	kind = c('binary', 'continuous', 'categorical'),
+	stringsAsFactors = FALSE
 )
 
-column_transformer <- sk_compose$ColumnTransformer(transformers)
+create_column_transformer <- function(feature_mapping){
+	transformers <- list()
+	for(i in 1:nrow(feature_mapping)){
+		parent_feature <- feature_mapping[i, 'parent_feature']
+		kind <- feature_mapping[i, 'kind']
+		if(kind == 'binary'){
+			transformer <- 'passthrough'
+		}else if(kind == 'categorical'){
+			transformer <- sk_preprocessing$OneHotEncoder()
+		}else if(kind == 'continuous'){
+			transformer <- sk_preprocessing$StandardScaler()
+		}else{
+			stop(glue('kind {kind} not supported'))
+		}
+		transformers[[i]] <- c(parent_feature, transformer, tuple(parent_feature))
+	}
+	return(sk_compose$ColumnTransformer(transformers))
+}
 
+column_transformer <- create_column_transformer(feature_mapping)
 column_transformer$fit_transform(df)
 
 get_transformer_column_order <- function(column_transformer){
@@ -47,35 +63,32 @@ get_transformer_column_order <- function(column_transformer){
 decompose_column_transformer <- function(column_transformer){
 	strategies <- list()
 	for(col in get_transformer_column_order(column_transformer)){
-		if(col != 'remainder'){
+		trans <- column_transformer$named_transformers_[[col]]
+		trans_class <- as.character(trans)
 
-			trans <- column_transformer$named_transformers_[[col]]
-			trans_class <- as.character(trans)
-
-			if(trans_class == 'OneHotEncoder()'){
-				strategies[[col]] <- list(
-					kind = 'categorical',
-					strategy = 'one-hot-encode',
-					levels = unlist(trans$categories_),
-					feature_names = str_replace_all(unlist(trans$get_feature_names()), 'x0_', glue('{col}_'))
-				)
-			}else if(trans_class == 'StandardScaler()'){
-				strategies[[col]] <- list(
-					kind = 'continuous',
-					strategy = 'scale',
-					mean = trans$mean_,
-					scale = trans$scale_,
-					var = trans$var_,
-					n_samples_seen = trans$n_samples_seen_,
-					feature_names = c(col)
-				)
-			}else if(trans_class == 'passthrough'){
-				strategies[[col]] <- list(
-					kind = NA,
-					strategy = 'passthrough',
-					feature_names = c(col)
-				)
-			}
+		if(trans_class == 'OneHotEncoder()'){
+			strategies[[col]] <- list(
+				kind = 'categorical',
+				strategy = 'one-hot-encode',
+				levels = unlist(trans$categories_),
+				feature_names = str_replace_all(unlist(trans$get_feature_names()), 'x0_', glue('{col}_'))
+			)
+		}else if(trans_class == 'StandardScaler()'){
+			strategies[[col]] <- list(
+				kind = 'continuous',
+				strategy = 'scale',
+				mean = trans$mean_,
+				scale = trans$scale_,
+				var = trans$var_,
+				n_samples_seen = trans$n_samples_seen_,
+				feature_names = c(col)
+			)
+		}else if(trans_class == 'passthrough'){
+			strategies[[col]] <- list(
+				kind = NA,
+				strategy = 'passthrough',
+				feature_names = c(col)
+			)
 		}
 	}
 	return(strategies)
@@ -110,3 +123,5 @@ column_transform <- function(df, strategies){
 
 R_result_X <- column_transform(df, strategies)
 python_result_X <- column_transformer$transform(df)
+
+all(R_result_X == python_result_X)
